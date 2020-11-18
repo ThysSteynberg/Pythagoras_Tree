9cc6d9be3389ccf21d8025823b56a7d5c05321ce
asteynberg <58010639+asteynberg@users.noreply.github.com>
2020-11-18 14:11:20 +0200

    Added Documentation

--- pythagorean tree.py
+++ pythagorean tree.py
@@ -2,13 +2,16 @@
 """
 Created on Tue Nov 17 13:54:41 2020
 
-@author: Thys
+@author: Thys Steynberg
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+"""
+Box class. Contains left and right "bottom" vertices, the length of one side
+and the angle that it is rotated with respect to the x-axis.
+"""
 
 class Box():
     def __init__(self, left, right, length, angle):
@@ -16,7 +19,6 @@ class Box():
         self.right = right
         self.angle = angle
         self.legth = length
-        
     def getLeft(self):
         return self.left
     def getRight(self):
@@ -39,20 +41,28 @@ Then apply it to those two boxes
 """     
 def recurPythTree(box, count):
     if count>0:
+        #Get the coordinates of the four vertices + the initial vertex to create a full plot
         coords = np.array([box.getLeft(), box.getRight(),box.getRight()+R_matrix([[0],[box.getLen()]],box.getAngle() ),box.getLeft()+R_matrix([[0],[box.getLen()]],box.getAngle() ), box.getLeft()])
         xc = [x[0] for x in coords]
         yc = [y[1] for y in coords]
         plt.plot(xc, yc,color='green',linewidth=0.3*box.getLen())
+        #Create the "left" box
         box2 = Box(box.getLeft()+R_matrix([[0],[box.getLen()]],abs(box.getAngle())),box.getLeft()+R_matrix([[0],[box.getLen()]],abs(box.getAngle()))+R_matrix([[box.getLen()*(1/np.sqrt(2))],[0]],abs(box.getAngle())+np.pi/4), box.getLen()*(1/np.sqrt(2)), abs(box.getAngle())+np.pi/4)
         recurPythTree(box2, count-1)
+        #Create the "right" box
         box3 = Box(box.getLeft()+R_matrix([[0],[box.getLen()]],box.getAngle())+R_matrix([[box.getLen()*(1/np.sqrt(2))],[0]],box.getAngle()+1*np.pi/4), box.getRight()+R_matrix([[0], [box.getLen()]],box.getAngle()),box.getLen()*(1/np.sqrt(2)), box.getAngle()+7*np.pi/4)
         recurPythTree(box3, count-1)
         
-
+"""
+Set up figure axis
+"""
 fig, ax = plt.subplots()  
 plt.figure(dpi=2000) 
 ax.set_aspect(1)
-box1 = Box(np.array([[0],[0]]), np.array([[10],[0]]), 10, 0)
 
+"""
+Initial call to tree function
+"""
+box1 = Box(np.array([[0],[0]]), np.array([[10],[0]]), 10, 0)
 recurPythTree(box1,20)
 plt.show()
